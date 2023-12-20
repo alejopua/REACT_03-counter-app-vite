74ae43972d651802354f1179de1d75f3c6ad16a4
alejopua <alejopua@gmail.com>
2023-12-20 11:19:17 -0500

    (add)[seccion 4]: Class 6 - Insert style with tailwind css in jsx

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -6,8 +6,8 @@ const newName = (name) => {
 export const FirstApp = () => {
   return (
     <>
-      <h1>{newName('Alejandro')}</h1>
-      <h3>My first React App :)</h3>
+      <h1 className="text-3xl font-bold underline">{newName('Alejandro')}</h1>
+      <h3 className="bg-orange-400">My first React App :)</h3>
     </>
   )
 }
