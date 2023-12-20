1ab202f5ad84c36d5da45149ddaa90758cc709ac
alejopua <alejopua@gmail.com>
2023-12-19 20:44:28 -0500

    (add)[seccion 4]: Class 2 - Our first component

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-export function App() {
-  return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
-  )
-}
+import { HelloWorldApp } from './HelloWorldApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelloWorldApp />
   </React.StrictMode>
 )
 
