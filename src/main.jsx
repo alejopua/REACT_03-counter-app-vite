8629c72b4ca0cde70e6cfb85f10639b8d2b92742
alejopua <alejopua@gmail.com>
2023-12-19 20:35:35 -0500

    (add)[seccion 4]: Class 1 - Hello world in React

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+export function App() {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
