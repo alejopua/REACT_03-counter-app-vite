cde21266e1bbaa17f7737d2898ac7b3c71542fc2
alejopua <alejopua@gmail.com>
2023-12-20 07:14:53 -0500

    (add)[seccion 4]: Class 5 - Insert variables in html with jsx

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,12 @@
 
+const newName = (name) => {
+  return `Hello ${name}`
+}
+
 export const FirstApp = () => {
   return (
     <>
-      <h1>FirstApp by alejopua</h1>
+      <h1>{newName('Alejandro')}</h1>
       <h3>My first React App :)</h3>
     </>
   )
