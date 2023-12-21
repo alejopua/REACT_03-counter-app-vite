d6917025af1982c7b6883e61ea623d1539279622
alejopua <alejopua@gmail.com>
2023-12-20 23:46:09 -0500

    (add)[seccion 5]: Class 3 - Example Jest - Expect - toBe

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,8 +1,16 @@
 
 // eslint-disable-next-line no-undef
-test('This test should pass', () => {
-  // eslint-disable-next-line no-constant-condition
-  if ( 0 === 1 ) {
-    throw new Error('This test failed!');
-  }
+describe('First Test in <DemoComponent />', () => {
+  // eslint-disable-next-line no-undef
+  test('This test should pass', () => {
+    // 1. inicialización
+    const msg1 = 'Hola Mundo';
+
+    //  2. estímulo
+    const msg2 = msg1.trim();
+
+    //  3. observar el comportamiento... esperado
+    // eslint-disable-next-line no-undef
+    expect( msg1 ).toBe( msg2 );
+  });
 });
