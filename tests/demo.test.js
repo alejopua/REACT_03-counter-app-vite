d4fa395e9d052a7622f69756091dcf9535f4dab4
alejopua <alejopua@gmail.com>
2023-12-20 23:21:58 -0500

    (add)[seccion 5]: Class 2 - First tests

---
+++ tests/demo.test.js
@@ -0,0 +1,8 @@
+
+// eslint-disable-next-line no-undef
+test('This test should pass', () => {
+  // eslint-disable-next-line no-constant-condition
+  if ( 0 === 1 ) {
+    throw new Error('This test failed!');
+  }
+});
